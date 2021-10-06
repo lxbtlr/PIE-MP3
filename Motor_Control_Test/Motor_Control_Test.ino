14ee66ecda7408a7831b379e3a5f3fc6e16c910d
Zeno Schwebel <zschwebel@users.noreply.github.com>
2021-10-06 10:01:06 -0400

    Update Motor_Control_Test.ino

--- Motor_Control_Test/Motor_Control_Test.ino
+++ Motor_Control_Test/Motor_Control_Test.ino
@@ -4,20 +4,35 @@
 Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
 Adafruit_DCMotor *myMotor_R = AFMS.getMotor(1);
 Adafruit_DCMotor *myMotor_L = AFMS.getMotor(2);
+int speed_L = 0;
+int speed_R = 0;
+int i = 0;
+int t = 0;
 
 void setup() {
-  
+  long baudRate = 115200;       // NOTE1: The baudRate for sending & receiving programs must match
+  Serial.begin(baudRate);     // NOTE2: Set the baudRate to 115200 for faster communication  
   AFMS.begin();
-  myMotor_L->setSpeed(50);
-  myMotor_R->setSpeed(50);
+  myMotor_L->setSpeed(speed_L);
+  myMotor_R->setSpeed(speed_R);
   
   // put your setup code here, to run once:
 
 }
 
 void loop() {
-  myMotor_L->run(FORWARD);
-  myMotor_R->run(FORWARD);
+  speed_L = 0;
+  speed_R = 0;
+  myMotor_L->setSpeed(speed_L);
+  myMotor_R->setSpeed(speed_R);
+  t = millis();
+  Serial.println(t);
+  if (i < t)
+    myMotor_L->run(FORWARD);
+    myMotor_R->run(FORWARD);
+    i = millis();
+  
   // put your main code here, to run repeatedly:
 
 }
+//function that takes in an angle and returns an 
