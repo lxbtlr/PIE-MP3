5e7f51d28ab192b421b1921ae218c0e6adf2a618
lxbtlr <abutler1@olin.edu>
2021-10-15 10:34:36 -0400

    line follower updated

--- LineFollower/LineFollower.ino
+++ LineFollower/LineFollower.ino
@@ -2,9 +2,10 @@ int leftMostSense = A0;
 int leftSense = A1;
 int rightSense = A2;
 int rightMostSense = A3;
+int nmax = 0;
 
 /*
-take in the sensor values - denoise them
+  take in the sensor values - denoise them
 
 */
 void setup() {
@@ -17,11 +18,47 @@ void setup() {
 }
 
 void loop() {
-  long sensorValue =0;
-  // put your main code here, to run repeatedly:
-  for(int i = 0; i<100;i++){
-    sensorValue += analogRead(rightMostSense);
-    }
-  sensorValue = sensorValue/100;
-  Serial.println(sensorValue);
+  int A = sq(analogRead(leftMostSense)/100);
+  int B = sq(analogRead(leftSense)/100);
+  int C = sq(analogRead(rightSense)/100);
+  int D = sq(analogRead(rightMostSense)/100);
+  Serial.print(A);
+  Serial.print(" ");
+  Serial.print(B);
+  Serial.print(" ");
+  Serial.print(C);
+  Serial.print(" ");
+  Serial.println(D);
+  
+//  nmax = max(A,B);
+//  nmax = max(nmax,C);
+//  nmax = max(nmax,D);
+//  Serial.println(nmax);
+
+
+
+//  Serial.print("A: ");
+//  if (A >= 800) {
+//    Serial.print("1 ");
+//  } else {
+//    Serial.print("0 ");
+//  }
+//  Serial.print("B: ");
+//  if (B >= 800) {
+//    Serial.print("1 ");
+//  } else {
+//    Serial.print("0 ");
+//  }
+//  Serial.print("C: ");
+//  if (C >= 800) {
+//    Serial.print("1 ");
+//  } else {
+//    Serial.print("0 ");
+//  }
+//  Serial.print("D: ");
+//  if (D >= 800) {
+//    Serial.println("1");
+//  } else {
+//    Serial.println("0");
+//  }
 }
